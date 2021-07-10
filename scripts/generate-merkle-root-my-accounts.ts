3f64e658bc5e1f34645ed71986fefe181ec003c1
arnoldlaishram <arnold@jiny.io>
2021-07-10 09:12:20 +0530

    added bignumber.js library to generate quadratic token distribution

--- scripts/generate-merkle-root-my-accounts.ts
+++ scripts/generate-merkle-root-my-accounts.ts
@@ -6,8 +6,6 @@ const wallet0 = '0x2c1929EE38950843211d1b22C31Ac18F5b23e0c0'
 const wallet1 = '0xd001c8ADAbf28128845f18871CE1346EC078eE92'
 import { generateMerkleRoot } from '../scripts/generate-merkle-root';
 
-const tokenSupply = 3000000000000000000;
-
 const dorgDao: Dao = {
 
     "avatarContract": {
@@ -16,23 +14,23 @@ const dorgDao: Dao = {
     },
     "name": "Avatar",
     "nativeReputation": {
-        "totalSupply": tokenSupply
+        "totalSupply": 100
     },
     "reputationHolders": [
         {
             "address": wallet0,
-            "balance": 1000000000000000000
+            "balance": 49
         },
         {
             "address": wallet1,
-            "balance": 1000000000000000000
+            "balance": 16
         }
     ]
 }
 
 async function generateMyAccountRoot() {
 
-    const merkleRoot = await generateMerkleRoot(`${tokenSupply}`, dorgDao)
+    const merkleRoot = await generateMerkleRoot("10000000000000000000000", dorgDao)
     console.log(JSON.stringify(merkleRoot, null, 2))
 
 }
--- scripts/generate-merkle-root.ts
+++ scripts/generate-merkle-root.ts
@@ -4,6 +4,9 @@ import { BigNumber, utils } from 'ethers'
 import BalanceTree from '../src/balance-tree'
 import * as path from 'path'
 import { writeToFile } from '../src/util'
+import { formatEther, parseEther } from 'ethers/lib/utils';
+const BN = require('bignumber.js');
+const DecimalBN = BN.BigNumber
 
 const { isAddress, getAddress } = utils
 
@@ -28,6 +31,7 @@ const xdaiQuery = gql`
     }
 `
 
+
 const fetchDOrgDaoReps = (endpoint: string, query: string, variables = {}) => new Promise<DorgGQLRes | any>((resolve, reject) => {
   request(endpoint, query, variables)
     .then((data) => resolve(data))
@@ -38,10 +42,14 @@ export async function generateMerkleRoot(totalTokenToDistribute: string, dOrgDao
 
   const { totalSupply } = dOrgDao.nativeReputation
 
+  console.log('totalSupply: ' + totalSupply)
 
-  let total: BigNumber = BigNumber.from(0);
+  let totalTokenWithDecimal = new DecimalBN(0)
+  let totalTokenWithoutDecimal = new DecimalBN(0)
   const reputationHolders = dOrgDao.reputationHolders.reduce<{ [address: string]: { tokenByRep: BigNumber } }>
     ((addressTORepMap, reputationHolder) => {
+
+      console.log('\n')
 
       let address = reputationHolder.address
       if (!isAddress(address)) {
@@ -50,26 +58,41 @@ export async function generateMerkleRoot(totalTokenToDistribute: string, dOrgDao
       const parsedAdd = getAddress(address)
       if (addressTORepMap[parsedAdd]) throw new Error(`Duplicate address: ${parsedAdd}`)
 
-      const repHolderBal = BigNumber.from(`${reputationHolder.balance}`)
-      console.log('repHolderBal: ' + repHolderBal.toString())
-      const totalRepSupply = BigNumber.from(`${totalSupply}`)
-      console.log('totalRepSupply: ' + totalRepSupply.toString())
+      const repHolderBal = new DecimalBN(`${reputationHolder.balance}`)
+      console.log('repHolderBal: ' + repHolderBal.toFixed())
+      const totalRepSupply = new DecimalBN(`${totalSupply}`)
+      console.log('totalRepSupply: ' + totalRepSupply.toFixed())
 
-      const wad = BigNumber.from('10000000000000000000000000000000000')
+      let repFraction = repHolderBal.dividedBy(totalRepSupply)
+      const repPercent = repFraction.multipliedBy(DecimalBN("100"))
 
-      let repFraction = repHolderBal.mul(wad).div(totalRepSupply)
-      console.log('repFraction: ' + repFraction.toString())
+      const wad = new DecimalBN('100000000000000000000000000000000000000000000000000') // 1e50
+      const wadRoot = new DecimalBN('10000000000000000000000000') // 1e25
+
+      const repPercentRoot = repPercent.multipliedBy(wad).squareRoot() // Multiple by wad not not lose any decimal
+      console.log('repPercentRoot: ' + repPercentRoot.toFixed())
+      repFraction = repPercentRoot.dividedBy(new DecimalBN("100"))
+      console.log('repFraction: ' + repFraction.toFixed())
+
       console.log('totalTokenToDistribute: ' + totalTokenToDistribute)
-      let token = repFraction.mul(BigNumber.from(totalTokenToDistribute)).div(wad)
+      const tokenToDistribute = new DecimalBN(totalTokenToDistribute)
+      let token = tokenToDistribute.multipliedBy(repFraction).dividedBy(wadRoot) // divide by root of wad
 
-      console.log('token: ' + token.toString())
-      total = total.add(token);
-      addressTORepMap[reputationHolder.address] = { tokenByRep: token }
+      console.log('tokenWithDecimal: ' + token.toFixed())
+      totalTokenWithDecimal = totalTokenWithDecimal.plus(token)
+      let tokenWithoutDecimal = token.toFixed().split('.')[0] // Remove the decimal, ethers.js BigNumber doesn't support Decimals
+      console.log('token: ' + tokenWithoutDecimal)
+      totalTokenWithoutDecimal = totalTokenWithoutDecimal.plus(new DecimalBN(tokenWithoutDecimal))
+      
+      addressTORepMap[reputationHolder.address] = { tokenByRep: BigNumber.from(tokenWithoutDecimal) }
 
       return addressTORepMap
     }, {})
 
-  console.log('total: ' + total.toString())
+  console.log('\nTotal Token to distribute ' + totalTokenToDistribute)
+  console.log('Total Token with Decimal: ' + totalTokenWithDecimal.toFixed())
+  console.log('Total Token without Decimal: ' + totalTokenWithoutDecimal.toFixed())
+  console.log('Total Token decimal difference: ' + totalTokenWithDecimal.minus(totalTokenWithoutDecimal).toFixed())
 
   const sortedAddresses = Object.keys(reputationHolders).sort()
 
